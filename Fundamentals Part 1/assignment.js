230dfe3f533f1efb8793bd6bfac0ca63380a2b45
Joel Wood <joelmwood@live.com>
2022-08-26 06:49:17 -0500

    Finished JS Fundamental Part 1

---
+++ Fundamentals Part 1/assignment.js
@@ -0,0 +1,23 @@
+        // 1.-----------------------------
+        let x = 5,
+            y = 6;
+        // alert(x + y);
+        //console.log(x + y);
+
+        // 2.------------------------------
+        // alert(1+2+3+4+5+6+7+8+9);
+
+        // 3.-----------------------------
+        let num = (4 + 6 + 9) / 77;
+        // console.log(num);
+
+        // 4.-----------------------------
+        let a = 10;
+        console.log(a);
+        console.log(9 * a);
+        let b = 7 * a;
+        console.log(b);
+        const max = 57;
+        let actual = max - 13;
+        let percentage = actual / max;
+        console.log(percentage);
